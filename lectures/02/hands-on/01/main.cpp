8e1ac8591d350fddbb8d2f2199ed5ca48058a4d2
guanhuashu <guanhuashu@gmail.com>
2022-01-23 22:59:11 -0500

    finished lecture 1 and 2 exercises.

--- lectures/02/hands-on/01/main.cpp
+++ lectures/02/hands-on/01/main.cpp
@@ -1,10 +1,46 @@
 // File       : main.cpp
 // Description: Lecture 2: Hands-On 01
 #include <iostream>
+using namespace std;
 // you can write to stdout with: std::cout << "Hello\n"; // (example)
 
+/**
+* Generate an integer array `v1` of size `N=100`
+* Write and call a function `init(...)` that initializes an input array `v` to
+  values `v[i] = i` for `i = 0, ..., N-1`
+* Create another array `v2` of size `N`
+* Write and call a function `cp(...)` that takes two input arrays and copies the
+  contents of the first array into the second array
+* Output the _difference_ `v1[i] - v2[i]` for `i = 0, ..., N-1` to the console
+  to test your implementation
+  **/
+
+void init(int v[], int N)
+{
+    for (int i=0; i<N; ++i) {
+        v[i] = i;
+    }
+}
+
+void cp(const int src[], int dst[], int N) 
+{
+    for (int i=0; i<N; ++i) {
+        dst[i] = src[i];
+    }
+}
+
+
+#define N 100
 int main(void)
 {
+    int v1[N];
+    int v2[N];
 
+    init(v1, N);
+    cp(v1, v2, N);
+   
+    for (int i=0; i<N; ++i) {
+        cout << v1[i] - v2[i] << "\n";
+    }
     return 0;
 }
--- lectures/02/hands-on/02/main.cpp
+++ lectures/02/hands-on/02/main.cpp
@@ -2,23 +2,31 @@
 // Description: Lecture 2: Hands-On 02, pointers
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
     int a[100]; // array with 100 elements
 
     // TODO: create a pointer that points to the first element in the array `a`
+    int *p0 = &a[0];
 
     // TODO: print the address of the pointer to stdout. (You can use std::cout
     // to print to stdout.)
+    cout << "Address of the pointer: " << &p0 << endl;
+
 
     // TODO: print the address the pointer is pointing to, to stdout.
+    cout << "Address the pointer points to: " << p0 << endl;
 
     // TODO: create another pointer that points to the last element in the array
     // `a`
+    int *p1 = &a[99];
 
     // TODO: print the difference between the address the pointer you just
     // created points to and the address the first pointer points to.  What do
     // you observe?
+    cout << "Difference p1 - p0: " << p1 - p0 << endl;
 
     return 0;
 }
